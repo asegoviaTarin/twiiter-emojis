8f3af172c2fb9e76a446fb6a16d33f94e2fc90bf
Alberto Segovia <axseta@gmail.com>
2023-05-24 21:06:40 +0200

    keep alive db

---
+++ src/scripts/keep-alive.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+ const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:["query", "error", "warn"]
+  });
+
+const runQuery = async function() {
+  // const result = await prisma.post.findMany({ take: 100, orderBy: [{ createdAt: "desc" }] })
+  await prisma.post.create({ data :{ authorId: 'user_2PQvKL1HvZ4dyalz0bGOfjP7EPX', content:'⏳' }})
+
+}
+const start = async function() {
+ await runQuery();
+}
+
+// Call start
+start();
+
